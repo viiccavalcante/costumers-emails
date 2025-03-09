5df3bf534bf6128273b6be21803d8b0573664805
victoria <victoriapcavalcante@gmail.com>
2025-03-09 13:40:30 +0100

    initial commit

---
+++ src/main/java/com/example/costumers_emails/CostumersEmailsApplication.java
@@ -0,0 +1,13 @@
+package com.example.costumers_emails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CostumersEmailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CostumersEmailsApplication.class, args);
+	}
+
+}
